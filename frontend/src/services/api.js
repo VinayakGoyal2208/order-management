39133d1eb0b212e7c9fea38c90f125e7d6279740
Vinayak <vinayakgoyal2208@gmail.com>
2026-04-27 17:27:48 +0530

    added backend render link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
+const BASE_URL = window.location.hostname === "localhost" 
+  ? "http://localhost:5000/api" 
+  : "https://order-management-backend-uqtn.onrender.com";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
   return req;
 });
 
